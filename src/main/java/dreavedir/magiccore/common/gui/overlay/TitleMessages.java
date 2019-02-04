c614053e9c5841a9304d67ca229b0bca826651f6
yknomeh <yknomeh@protonmail.com>
2019-02-04 21:56:23 +0100

    scaling fix

--- src/main/java/dreavedir/magiccore/common/gui/overlay/TitleMessages.java
+++ src/main/java/dreavedir/magiccore/common/gui/overlay/TitleMessages.java
@@ -14,12 +14,12 @@ public class TitleMessages extends Gui {
 
         GL11.glPushMatrix();
         GL11.glScalef(scaleX, scaleY ,scaleZ);
-        drawCenteredString(mc.fontRenderer, message, (width / 2) / scaleX, ((height / 2) - 50) / scaleY, Integer.parseInt("0b9902", 16));
+        drawCenteredString(mc.fontRenderer, message, (width / 2) / scaleX, ((height / 2) - 25 * scaleY) / scaleY, Integer.parseInt("0b9902", 16));
         GL11.glPopMatrix();
 
         GL11.glPushMatrix();
-        GL11.glScalef((float) scaleX / 2, (float) scaleY / 2, scaleZ);
-        drawCenteredString(mc.fontRenderer, submessage, (width / 2) / (int)((float) scaleX / 2), ((height / 2) - 30) / (int)((float) scaleY / 2), Integer.parseInt("0b9902", 16));
+        GL11.glScalef(scaleX / 2, scaleY / 2, scaleZ);
+        drawCenteredString(mc.fontRenderer, submessage, (width / 2) / (scaleX / 2), (((height / 2) - (25 * ( scaleY / 2) ))) / (int)((float) scaleY / 2), Integer.parseInt("0b9902", 16));
         GL11.glPopMatrix();
     }
 
